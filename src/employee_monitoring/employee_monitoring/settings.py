b76f225e116968d1481717e9ec44e5081755fd44
psychx <#_#>
2024-06-09 17:01:37 +0300

    add apps

--- src/employee_monitoring/employee_monitoring/settings.py
+++ src/employee_monitoring/employee_monitoring/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employee.apps.EmployeeConfig',
+    'reservation.apps.ReservationConfig',
+    'manager.apps.ManagerConfig',
 ]
 
 MIDDLEWARE = [
